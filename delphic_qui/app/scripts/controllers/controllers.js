209c4abab19846c8419c7b424e9eadaa5378c111
Michael Ishmael <michaelishmael1976@gmail.com>
2015-01-16 01:20:51 +0000

    Update to extraction UI

--- delphic_qui/app/scripts/controllers/controllers.js
+++ delphic_qui/app/scripts/controllers/controllers.js
@@ -12,35 +12,8 @@ app.config(['$routeProvider', '$httpProvider', 'fileUploadProvider', function ($
         when('/extraction', {
             templateUrl: '/views/extractionViewer.html',
             controller: 'ExtractionCtrl'
-        }).
-        when('/extraction/list', {
-            controller: 'ListCtrl',
-            resolve: {
-                companies: ["MultiCompanyLoader", function (MultiCompanyLoader) {
-                    return MultiCompanyLoader.list();
-                }]
-            },
-            templateUrl: '/views/list.html'
-        }).when('/extraction/edit/:companyId', {
-            controller: 'EditCtrl',
-            resolve: {
-                company: ["CompanyLoader", function (CompanyLoader) {
-                    return CompanyLoader();
-                }]
-            },
-            templateUrl: '/views/companyForm.html'
-        }).when('/extraction/view/:companyId', {
-            controller: 'ViewCtrl',
-            resolve: {
-                company: ["CompanyLoader", function (CompanyLoader) {
-                    return CompanyLoader();
-                }]
-            },
-            templateUrl: '/views/viewCompany.html'
-        }).when('/extraction/new', {
-            controller: 'NewCtrl',
-            templateUrl: '/views/companyForm.html'
-        }).otherwise({redirectTo: '/'});
+        })
+        .otherwise({redirectTo: '/'});
 
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
     fileUploadProvider.defaults.redirect = window.location.href.replace(
@@ -52,6 +25,8 @@ app.config(['$routeProvider', '$httpProvider', 'fileUploadProvider', function ($
 
 app.controller('appCtrl', ['$scope', '$location', 'MultiCompanyLoader',
     function($scope, $location, MultiCompanyLoader){
+
+        $scope.listQuery = {top: 20, filter: ''};
 
         $scope.isActive = function (viewLocation){
             return viewLocation === $location.path();
@@ -93,36 +68,236 @@ app.controller('appCtrl', ['$scope', '$location', 'MultiCompanyLoader',
 app.controller('CompanyUploadCtrl', ['$scope',
         function($scope){
 
-            $scope.top = 20;
-            $scope.filter = '';
-
-            $scope.loadCompanies($scope.top, null);
-
-            $scope.$watch('top', updateList);
-            $scope.$watch('filter', updateList);
-
-            function updateList(){
-                $scope.loadCompanies($scope.top, $scope.filter);
-            }
-
-            $scope.companyToAdd = {}
+            $scope.companyToAdd = {};
 
             $scope.addCompany = function(){
                 if($scope.companyToAdd.name && $scope.companyToAdd.registerednumber)
                     $scope.addNewCompany($scope.companyToAdd);
+            };
+
+            $scope.$watch('listQuery.top', updateList);
+            $scope.$watch('listQuery.filter', updateList);
+
+            function updateList(){
+                $scope.loadCompanies($scope.listQuery.top, $scope.listQuery.filter);
             }
 
+            $scope.loadCompanies($scope.listQuery.top, null);
 
-        }]
-    );
+        }
+    ]);
 
 app.controller('ExtractionCtrl', ['$scope',
-    function($scope){
+    function($scope) {
 
+        $scope.selections = {selectedIndex: null, selectedUrl: '', linkHistory: []};
+
+        $scope.selectCompany = function (idx, id) {
+            $scope.selections.selectedIndex = idx;
+        };
+
+        $scope.$watch('listQuery.top', updateList);
+        $scope.$watch('listQuery.filter', updateList);
+
+        function updateList(){
+            $scope.loadCompanies($scope.listQuery.top, $scope.listQuery.filter);
+        }
+
+        $scope.loadCompanies($scope.listQuery.top, null);
+
+    }
+]);
+
+app.controller('CompanyListController',
+    function ($scope) {
+
+        $scope.repo = CompanyRepository;
+        $scope.companies = [];
+
+        $scope.$watch('repo.companies', function(){
+            $scope.companies = $scope.repo.companies;
+        });
+
+        MultiCompanyLoader.list().then(function (companies) {
+            $scope.repo.companies = companies;
+        });
+
+        $scope.selectCompany = function (idx, id) {
+            $scope.repo.selectedIndex = idx;
+        }
+    }
+);
+
+app.controller('ManageCompanyController', function ($scope, CompanyWebsiteLocator) {
+
+    $scope.$watch('selections.selectedIndex', function () {
+        if ($scope.selections.selectedIndex > -1
+            && $scope.companies.length > $scope.selections.selectedIndex) {
+            $scope.company = $scope.companies[$scope.selections.selectedIndex]
+        }
+    });
+
+    $scope.getWebsiteUrl = function (id) {
+        $scope.websiteStatus = 'Finding website...';
+        var promise = CompanyWebsiteLocator.getWebsiteUrl(id);
+        promise.then(function (payload) {
+                $scope.company.website = payload;
+                if(payload) $scope.selections.selectedUrl = payload;
+                $scope.websiteStatus = null;
+            },
+            function (errorPayload) {
+                $scope.websiteStatus = errorPayload;
+            }
+        );
+    };
+
+    $scope.loadSiteData  = function() {
+        getWebsiteMetaTags();
+        getWebsiteLinks();
+    };
+
+    $scope.setAnalyseUrl = function (url) {
+      $scope.selectedUrl = url;
+    };
+
+    function getWebsiteMetaTags () {
+        $scope.pageLoadStatus = 'Finding metadata...';
+        var promise = CompanyWebsiteLocator.getWebsiteMeta($scope.company.website);
+        promise.then(function (payload) {
+                $scope.company.metatags = payload;
+                $scope.pageLoadStatus = null;
+            },
+            function (errorPayload) {
+                $scope.pageLoadStatus = errorPayload;
+            }
+        );
+    };
+
+    function getWebsiteLinks() {
+        $scope.pageLoadStatus += '<br>Finding top level links...';
+        var promise = CompanyWebsiteLocator.getWebsiteLinks($scope.company.website);
+        promise.then(function (payload) {
+                $scope.company.websitelinks = payload;
+                $scope.websiteLinkStatus = null;
+            },
+            function (errorPayload) {
+                $scope.websiteLinkStatus = errorPayload;
+            }
+        );
+    }
+
+
+});
+
+
+app.controller('CompanyMetadataController', function ($scope, CompanyPageResults) {
+
+    $scope.pageUrl = '';
+
+    $scope.$watch('selections.selectedUrl', function () {
+        $scope.pageUrl = $scope.selections.selectedUrl;
+        $scope.loadPageResults();
+    });
+
+    $scope.loadPageResults  = function() {
+        if(!$scope.pageUrl) return;
+        getPageCalais();
+        //getWebsiteLinks();
+    };
+
+    function getPageCalais () {
+        $scope.status = 'Finding page Open Calais results...';
+        var promise = CompanyPageResults.getCalaisTags($scope.pageUrl);
+        promise.then(function (payload) {
+                $scope.calaisTags = payload;
+                $scope.status = null;
+            },
+            function (errorPayload) {
+                $scope.status = errorPayload;
+            }
+        );
+    }
+
+    function getWebsiteLinks() {
+        $scope.websiteStatus += '<br>Finding top level links...';
+        var promise = CompanyPageResults.getWebsiteLinks($scope.company.website);
+        promise.then(function (payload) {
+                $scope.company.websitelinks = payload;
+                $scope.websiteLinkStatus = null;
+            },
+            function (errorPayload) {
+                $scope.websiteLinkStatus = errorPayload;
+            }
+        );
+    }
+
+
+});
+
+
+app.controller('DemoFileUploadController', [
+    '$scope', '$http', '$filter', '$window',
+    function ($scope, $http) {
+        var url = 'http://127.0.0.1:8000/upload/uploadcompanyfilex';
+        $scope.options = {
+            url: url
+        };
+
+        //$scope.getList = function(){
+            $http.get(url)
+                .then(
+                function (response) {
+                    $scope.loadingFiles = false;
+                    $scope.queue = response.data.files || [];
+                },
+                function () {
+                    $scope.loadingFiles = false;
+                }
+            );
+        //};
+
+        $scope.loadingFiles = false;
 
 
     }
 ]);
+
+
+
+/*
+app.controller('FileDestroyController', [
+    '$scope', '$http',
+    function ($scope, $http) {
+        var file = $scope.file,
+            state;
+        if (file.url) {
+            file.$state = function () {
+                return state;
+            };
+            file.$destroy = function () {
+                state = 'pending';
+                return $http({
+                    url: file.deleteUrl,
+                    method: file.deleteType
+                }).then(
+                    function () {
+                        state = 'resolved';
+                        $scope.clear(file);
+                    },
+                    function () {
+                        state = 'rejected';
+                    }
+                );
+            };
+        } else if (!file.$cancel && !file._index) {
+            file.$cancel = function () {
+                $scope.clear(file);
+            };
+        }
+    }
+]);
+
+
 
 app.controller('ListCtrl', ['$scope', 'companies', 'CompanyWebsiteLocator',
     function ($scope, companies, CompanyWebsiteLocator) {
@@ -182,190 +357,4 @@ app.controller('NewCtrl', ['$scope', '$location', 'Company',
         };
     }]);
 
-app.controller('CompanyListController', function ($scope, CompanyRepository, MultiCompanyLoader) {
-
-        $scope.repo = CompanyRepository;
-        $scope.companies = [];
-
-        $scope.$watch('repo.companies', function(){
-            $scope.companies = $scope.repo.companies;
-        });
-
-        MultiCompanyLoader.list().then(function (companies) {
-            $scope.repo.companies = companies;
-        });
-
-        $scope.selectCompany = function (idx, id) {
-            $scope.repo.selectedIndex = idx;
-        }
-    }
-);
-
-app.controller('ManageCompanyController', function ($scope, CompanyRepository, CompanyWebsiteLocator) {
-
-    $scope.repo = CompanyRepository;
-
-    $scope.$watch('repo.selectedIndex', function () {
-        if ($scope.repo.selectedIndex > -1
-            && $scope.repo.companies.length > $scope.repo.selectedIndex) {
-            $scope.company = $scope.repo.companies[CompanyRepository.selectedIndex]
-        }
-    });
-
-    $scope.getWebsiteUrl = function (id) {
-        $scope.websiteStatus = 'Finding website...';
-        var promise = CompanyWebsiteLocator.getWebsiteUrl(id);
-        promise.then(function (payload) {
-                $scope.company.website = payload;
-                if(payload) $scope.repo.selectedUrl = payload;
-                $scope.websiteStatus = null;
-            },
-            function (errorPayload) {
-                $scope.websiteStatus = errorPayload;
-            }
-        );
-    };
-
-    $scope.loadSiteData  = function() {
-        getWebsiteMetaTags();
-        getWebsiteLinks();
-    };
-
-    $scope.setAnalyseUrl = function (url) {
-      $scope.repo.selectedUrl = url;
-    };
-
-    function getWebsiteMetaTags () {
-        $scope.websiteStatus = 'Finding metadata...';
-        var promise = CompanyWebsiteLocator.getWebsiteMeta($scope.company.website);
-        promise.then(function (payload) {
-                $scope.company.metatags = payload;
-                $scope.websiteStatus = null;
-            },
-            function (errorPayload) {
-                $scope.websiteStatus = errorPayload;
-            }
-        );
-    };
-
-    function getWebsiteLinks() {
-        $scope.websiteStatus += '<br>Finding top level links...';
-        var promise = CompanyWebsiteLocator.getWebsiteLinks($scope.company.website);
-        promise.then(function (payload) {
-                $scope.company.websitelinks = payload;
-                $scope.websiteLinkStatus = null;
-            },
-            function (errorPayload) {
-                $scope.websiteLinkStatus = errorPayload;
-            }
-        );
-    }
-
-
-});
-
-
-app.controller('CompanyMetadataController', function ($scope, CompanyRepository, CompanyPageResults) {
-
-    $scope.repo = CompanyRepository;
-    $scope.pageUrl = '';
-
-    $scope.$watch('repo.selectedUrl', function () {
-        $scope.pageUrl = $scope.repo.selectedUrl;
-        $scope.loadPageResults();
-    });
-
-    $scope.loadPageResults  = function() {
-        if(!$scope.pageUrl) return;
-        getPageCalais();
-        //getWebsiteLinks();
-    }
-
-    function getPageCalais () {
-        $scope.status = 'Finding page Open Calais results...';
-        var promise = CompanyPageResults.getCalaisTags($scope.pageUrl);
-        promise.then(function (payload) {
-                $scope.calaisTags = payload;
-                $scope.status = null;
-            },
-            function (errorPayload) {
-                $scope.status = errorPayload;
-            }
-        );
-    };
-
-    function getWebsiteLinks() {
-        $scope.websiteStatus += '<br>Finding top level links...';
-        var promise = CompanyPageResults.getWebsiteLinks($scope.company.website);
-        promise.then(function (payload) {
-                $scope.company.websitelinks = payload;
-                $scope.websiteLinkStatus = null;
-            },
-            function (errorPayload) {
-                $scope.websiteLinkStatus = errorPayload;
-            }
-        );
-    }
-
-
-});
-
-
-app.controller('DemoFileUploadController', [
-    '$scope', '$http', '$filter', '$window',
-    function ($scope, $http) {
-        var url = 'http://127.0.0.1:8000/upload/uploadcompanyfilex';
-        $scope.options = {
-            url: url
-        };
-
-        //$scope.getList = function(){
-            $http.get(url)
-                .then(
-                function (response) {
-                    $scope.loadingFiles = false;
-                    $scope.queue = response.data.files || [];
-                },
-                function () {
-                    $scope.loadingFiles = false;
-                }
-            );
-        //};
-
-        $scope.loadingFiles = false;
-
-
-    }
-]);
-
-app.controller('FileDestroyController', [
-    '$scope', '$http',
-    function ($scope, $http) {
-        var file = $scope.file,
-            state;
-        if (file.url) {
-            file.$state = function () {
-                return state;
-            };
-            file.$destroy = function () {
-                state = 'pending';
-                return $http({
-                    url: file.deleteUrl,
-                    method: file.deleteType
-                }).then(
-                    function () {
-                        state = 'resolved';
-                        $scope.clear(file);
-                    },
-                    function () {
-                        state = 'rejected';
-                    }
-                );
-            };
-        } else if (!file.$cancel && !file._index) {
-            file.$cancel = function () {
-                $scope.clear(file);
-            };
-        }
-    }
-]);
+*/
